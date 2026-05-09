74b4141ecaab885a30de67ac6f7e1c93095f3b22
LiuCho123 <l.cabello@duocuc.cl>
2026-05-08 22:03:00 -0400

    Subida Microservicio Productos

--- servicios-backend/servicio-productos/src/main/java/com/example/servicioproductos/Controller/ProductoController.java
+++ servicios-backend/servicio-productos/src/main/java/com/example/servicioproductos/Controller/ProductoController.java
@@ -1,4 +1,56 @@
 package com.example.servicioproductos.Controller;
 
+import com.example.servicioproductos.Model.Producto;
+import com.example.servicioproductos.Service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public List<Producto> listar(){
+        return productoService.listar();
+    }
+
+    @GetMapping("/vendedor/{vendedorId}")
+    public List<Producto> listarPorVendedor(@PathVariable Long vendedorId){
+        return productoService.listarPorVendedor(vendedorId);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Producto> crear (@RequestBody Producto producto){
+        return ResponseEntity.ok(productoService.guardar(producto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Producto> obtenerPorId (@PathVariable Long id){
+        return ResponseEntity.ok(productoService.obtenerPorId(id));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id){
+        productoService.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/estado")
+    public ResponseEntity<Producto> cambiarEstado(@PathVariable Long id, @RequestParam boolean activo){
+        return ResponseEntity.ok(productoService.cambiarEstadoActivo(id, activo));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Producto> actualizar(@PathVariable Long id, @RequestBody Producto producto){
+        producto.setId(id);
+        return ResponseEntity.ok(productoService.guardar(producto));
+    }
 }
