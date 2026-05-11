a95bdb2b8d791a7a3e277759d61d8451e07b0d41
FrancoM27 <fr.millar@duocuc.cl>
2026-05-11 16:40:30 -0400

    agregue los cambios de seguridad a los servicios que faltaban

---
+++ servicios-backend/servicio-pedidos/src/main/java/com/gamebakes/servicio_pedidos/Security/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.gamebakes.servicio_pedidos.Security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            try {
+                String username = jwtUtils.getSubjectFromToken(token);
+                String rol = jwtUtils.getRolFromToken(token);
+
+                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>());
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            } catch (Exception e) {
+                logger.error("Error validando el JWT: " + e.getMessage());
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- servicios-backend/servicio-usuarios/src/main/java/com/gamebakes/serviciousuarios/Controller/UsuarioController.java
+++ servicios-backend/servicio-usuarios/src/main/java/com/gamebakes/serviciousuarios/Controller/UsuarioController.java
@@ -6,7 +6,7 @@ import com.gamebakes.serviciousuarios.DTO.UsuarioDTO;
 import com.gamebakes.serviciousuarios.Model.Usuario;
 import com.gamebakes.serviciousuarios.Service.UsuarioService;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
+//import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
