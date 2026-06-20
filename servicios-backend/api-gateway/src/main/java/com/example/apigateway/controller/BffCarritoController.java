b4ff08aa28f8860d1c5d3b6cab94258285eb0f4b
LiuCho123 <l.cabello@duocuc.cl>
2026-06-20 18:17:27 -0400

    Fix de IPs de Kafka

--- servicios-backend/api-gateway/src/main/java/com/example/apigateway/controller/BffCarritoController.java
+++ servicios-backend/api-gateway/src/main/java/com/example/apigateway/controller/BffCarritoController.java
@@ -3,15 +3,13 @@ package com.example.apigateway.controller;
 import com.example.apigateway.client.PagoClient;
 import com.example.apigateway.client.ProductoClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:5173", "http://18.211.231.0", "http://18.211.231.0:5173"}, allowCredentials = "true")
 @RestController
 @RequestMapping("/bff/carrito")
 public class BffCarritoController {
